ba4ced8edf9ed97f934f2d95c73eea0211350778
arthur-timoteo <arthur.timoteo@gmail.com>
2025-02-21 21:06:43 -0300

    feat: Create base assets endpoint

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
 
 @Module({
-  imports: [],
+  imports: [AssetsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
