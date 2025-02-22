1a09001a3e5a6fe4d5392a7fd204ceace11e0199
arthur <arthur.timoteo@gmail.com>
2025-02-21 22:24:08 -0300

    feat: Connect with database and create asset collection

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AssetsModule],
+  imports: [
+    AssetsModule,
+    MongooseModule.forRoot('mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true')
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
