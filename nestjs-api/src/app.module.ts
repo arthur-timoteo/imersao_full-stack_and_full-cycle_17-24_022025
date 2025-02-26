a3edc42e85057f3bfad54c107a2d95b3ec4cb857
arthur <arthur.timoteo@gmail.com>
2025-02-25 21:16:42 -0300

    feat: [API] Create base order endpoint

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { AssetsModule } from './assets/assets.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { WalletsModule } from './wallets/wallets.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
     AssetsModule,
     MongooseModule.forRoot('mongodb://root:root@localhost:27017/nest?authSource=admin&directConnection=true'),
-    WalletsModule
+    WalletsModule,
+    OrdersModule
   ],
   controllers: [AppController],
   providers: [AppService]
